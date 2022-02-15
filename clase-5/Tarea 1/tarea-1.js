07c8d78fe5ae8cc73ace31b8aee6e11701804f70
IsmaelFdezz <ismaelfifa@gmail.com>
2022-02-15 13:49:48 -0300

    Tarea 1

---
+++ clase-5/Tarea 1/tarea-1.js
@@ -0,0 +1,13 @@
+const $botonCalcular = document.querySelector("#calcular");
+
+$botonCalcular.onclick = function() {
+  const salarioAnual = Number(document.querySelector("#salario-anual").value);
+  const mesesAño = 12
+  var salarioMensual
+
+  salarioMensual = salarioAnual / mesesAño;
+
+  console.log(salarioMensual);
+  document.querySelector("#salario-mensual").value = salarioMensual
+  return false;
+}
